6a81f216aff3aa89bae98847a6cd01b632325987
Urbanizedeye <aidedow@gmail.com>
2020-10-23 12:30:16 +1100

    more things

--- project2/Group 3/getBookInfo.js
+++ project2/Group 3/getBookInfo.js
@@ -1,5 +1,9 @@
 //This will hold the functions and classes that get the bookDetails and Movie info; also handles finding the book that the user inputs
 
+//key to use the api of Movie Database
+// apiKey= "be0a41f9413fd3b245db2c906807e659";
+
+
 //This function finds the book the user inputs and puts all that it finds in the output Div
 function findBooks(){
     document.getElementById("output").innerHTML="";
@@ -8,17 +12,17 @@ function findBooks(){
     .then(Response =>{
         for(var i=0; i < Response.docs.length; i++){//this is the formating of the information gotten from openlibraries
             document.getElementById("output").innerHTML+=
-            "<div class='bookSearchResults'>"+
-            "<h2>"+Response.docs[i].title+"</h2>"+
-            "<h2>"+Response.docs[i].author_name[0]+"</h2>"+
-            "<h2>"+Response.docs[i].isbn[0]+"</h2>"+
-            "<br><img id='searchBookCover' src='http://covers.openlibrary.org/b/isbn/"+Response.docs[i].isbn[0]+"-M.jpg' alt="+"'No image exists"+"'><br>"+
+            "<div class='column' id='bookSearchResults'>"+
+            "<img id='searchBookCover' src='http://covers.openlibrary.org/b/isbn/"+Response.docs[i].isbn[0]+"-M.jpg' />"+
+            Response.docs[i].title+"<br>"+
+            Response.docs[i].author_name[0]+"<br>"+
+            Response.docs[i].isbn[0]+"<br>"+
             "</div>";
         }
     });
 }
 
-//this handles getting the cover to the book
+//this handles getting the cover to the book for book detail
 class getBookCover{
     //Returns the correct image Html for the given isbn number
 
@@ -26,7 +30,7 @@ class getBookCover{
         this.isbn = isbn;
         this.size = "-"+size;
         this.key = key.toLowerCase()+"/";
-        this.url_a = "<img src='http://covers.openlibrary.org/b/";
+        this.url_a = "<img id='BookdetailsBookcover' src='http://covers.openlibrary.org/b/";
         this.url_b = ".jpg' />";
     }
 
@@ -38,6 +42,43 @@ class getBookCover{
     display() {
         //returns the html directory in correct format
         return this.url_a + this.key + this.isbn + this.size + this.url_b;
+    }
+}
+
+//this class gets the movie details
+class movieDetail {
+    //handles getting all the information on a movie
+
+    constructor(name){
+        this.name = name;
+        this.api_key = "be0a41f9413fd3b245db2c906807e659";
+        this.url_a = "https://api.themoviedb.org/3/search/movie?api_key=";
+        this.url_b = "&language=en-US&query=";
+        this.url_c = "&page=1&include_adult=false";
+        this.poster_url_a = "<img id='moviePoster' src='http://image.tmdb.org/t/p/w200/";
+        this.poster_url_b = "' />";
+        this.detail = "";
+    }
+
+    async getDetail(){
+        let dets = await getMovieDetail(this.url_a, this.api_key, this.url_b, this.name, this.url_c);
+        this.detail = dets;
+    }
+
+    getName(){
+        return this.name
+    }
+
+    getReleaseDate(){
+        return this.detail['release_date']
+    }
+
+    getPosterImage(){
+        return this.poster_url_a+this.detail['poster_path']+this.poster_url_b
+    }
+
+    getAverageRating(){
+        return this.detail['vote_average']
     }
 }
 
@@ -83,11 +124,17 @@ class bookDetail {
         return this.detail['exerpts'][0]['text']
     }
 
+    getPublishDate(){
+        return this.detail['publish_date']
+    }
+
     getPageAmount(){
         //get amount of pages from json object
         return this.detail['number_of_pages']
     }
 }
+
+//this class gets movie details
 
 //get book details in the form of a json file
 async function getBookDetails(url_a, key, isbn, url_b){
@@ -97,10 +144,32 @@ async function getBookDetails(url_a, key, isbn, url_b){
     try {
         const resp = await fetch(url);
         const jres = await resp.json();
-        console.log(jres);
-
         return jres
     } catch (err){
+        throw err;
+    }
+}
+
+//gets a list of movies and finds the movie with the same name
+async function getMovieDetail(url_a, api_key,url_b, name, url_c){
+    //puts them in a this.detail file
+    let url = url_a+api_key+url_b+name+url_c;
+    try{
+        const resp = await fetch(url);
+        const jres = await resp.json();
+        console.log(jres.results);
+        let movie = "";
+        for(let x = 0; x < jres.results.length; x++){
+            let title = jres.results[x]["title"];
+            if(title.toLowerCase() == name.toLowerCase()){
+                movie = jres.results[x];
+                break;
+            }
+        }
+        console.log(movie);
+        return movie
+    } catch(err){
+
         throw err;
     }
 }
--- project2/Group 3/jsActivator.js
+++ project2/Group 3/jsActivator.js
@@ -6,6 +6,8 @@ function main(){
     
     
     findIsbnCorrelation()
+    findMovie();
+
     // tellIsbnKeys();
     // getBooksMovies();
 }
@@ -14,21 +16,47 @@ function main(){
 //dosn't like being in the same file as what it's calling since it's called first before activating the classes
 async function findIsbnCorrelation(){
     var bookarr = [];
+    var moviearr = [];
     if(isbnlist.length != 0){
         for(let i = 0; i < isbnlist.length; i++){
             let book = new bookDetail(isbnlist[i], "M");
             await book.getDetail();
+            let movie = new movieDetail(book.getTitle());
+            await movie.getDetail();
             bookarr.push(book);
+            moviearr.push(movie);
         }
-        document.getElementById("isbnResults").innerHTML +="The Results for the ISBN List";
+        document.getElementById("isbnResults").innerHTML +="The Results for the ISBN List</br>";
+        let y = 0;
         for(x of bookarr){
             document.getElementById("isbnResults").innerHTML +=
-            "<div class='bookDetails'>"+
+            "<div class='column' id='bookDetails'>"+
             x.cover()+
-            x.getAuthor()+
+            "<br>"+
+            "Author: "+x.getAuthor()+
+            "<br>"+
+            "Published: "+ x.getPublishDate()+
+            "<br>"+
+            "Pages: "+x.getPageAmount()+
             "</div>";
+
+            document.getElementById("isbnResults").innerHTML +=
+            "<div class='column' id='bookDetails'>"+
+            moviearr[y].getPosterImage()+
+            "<br>"+
+            moviearr[y].getName()+
+            "<br>"+
+            "Released: "+moviearr[y].getReleaseDate()+
+            "Average Rating: "+moviearr[y].getAverageRating()+
+            "</div>";
+            y++;
         }
     } else{
         document.getElementById("isbnResults").innerHTML = "There are no elements in the isbnList try with a new file."
     }
+}
+
+async function findMovie(){
+    let movie = new movieDetail("the hobbit");
+    await movie.getDetail();
 }
